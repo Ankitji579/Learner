297c9cb60ed86b37461578dbf5b94231cc5efaac
Ankit <dryisholeup@gmail.com>
2023-06-16 11:42:29 +0530

    Lesson 1

--- Big.java
+++ Big.java
@@ -1,1 +1,10 @@
-import java.util*;
+import java.util.*;
+public class Big{
+    public static void main(String [] args){
+        System.out.println("Write two numbers :");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println((a+b)/2);
+    }
+}
