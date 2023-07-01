ce6c2d7995b499c2ac8f27be408e586b27dbcb37
Ankit <dryisholeup@gmail.com>
2023-07-01 18:32:42 +0530

    this

--- Big.java
+++ Big.java
@@ -1,13 +1,12 @@
 import java.util.*;
 public class Big {
     public static void main(String Args[]){
-       /* double pi = 3.14;
+       /* double pi = 3.14; 
         System.out.println("Write the radius of cicles:");
         Scanner sc = new Scanner(System.in);
         float rad = sc.nextFloat();
         System.out.println("Perimeter of the circle is: " + (2 *pi* rad) );
         System.out.println("Area of the circle: " + (pi*rad*rad));
-        
         System.out.println("Write any two numbers:");
         Scanner sc = new Scanner(System.in);
         float num1 = sc.nextFloat(); 
